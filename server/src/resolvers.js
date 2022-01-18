a105d5b49f300dfaa045e88115757fe66817ae95
Raja Muhammad Asher <raja.muhammad.asher@gmail.com>
2022-01-18 18:23:48 +0500

    Complete Part 3

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,12 +4,18 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    track: (_, {id}, {dataSources}) => {
+      return dataSources.trackAPI.getTrack(id);
+    },
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-  },
+    modules: ({id}, _, {dataSources}) => {
+      return dataSources.trackAPI.getTrackModules(id);
+    },
+  },  
 };
 
 module.exports = resolvers;
